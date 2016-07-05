07d3b7239e8b67af68c2ee5369a12c2af50c9725
wan-tan <wan-tan@iamthanh.com>
2016-07-05 03:01:59 +0200

    First commit

---
+++ gulpfile.js
@@ -0,0 +1,85 @@
+var gulp = require('gulp')
+var notify = require('gulp-notify')
+var rename = require('gulp-rename')
+var rsync = require('gulp-rsync')
+var browserSync = require('browser-sync').create()
+var stylus = require('gulp-stylus')
+var htmlmin = require('gulp-htmlmin')
+var pump = require('pump')
+var concat = require('gulp-concat')
+var uglify = require('gulp-uglify')
+
+// ---------------------------------------------------------------- 
+// DEPLOY TO SERVER
+// ---------------------------------------------------------------- 
+gulp.task('deploy', function () {
+  gulp.src('build/**')
+    .pipe(rsync({
+      root: 'build',
+      hostname: 'ssh.iamthanh.com',
+      username: 'iamthanh.com',
+      progress: true,
+      destination: '/www/refugeeswork'
+    })
+      .pipe(notify({message: 'Deployed!'}))
+  )
+})
+
+// ---------------------------------------------------------------- 
+// STYLUS
+// ---------------------------------------------------------------- 
+gulp.task('stylus', function () {
+  return gulp.src('./src/stylus/*.styl')
+    .pipe(stylus({compress: true}))
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream())
+})
+
+// ---------------------------------------------------------------- 
+// HTML
+// ---------------------------------------------------------------- 
+gulp.task('html', function () {
+  return gulp.src('src/**/*.html')
+    .pipe(htmlmin({collapseWhitespace: true}))
+    .pipe(gulp.dest('build'))
+    .pipe(browserSync.stream())
+})
+
+// ---------------------------------------------------------------- 
+// JAVASCRIPT
+// ---------------------------------------------------------------- 
+gulp.task('js', function (cb) {
+  pump([
+    gulp.src('src/js/*.js'),
+    concat('bundle.min.js'),
+    uglify(),
+    gulp.dest('build/js'),
+    browserSync.stream()
+  ],
+    cb
+  )
+})
+
+// ---------------------------------------------------------------- 
+// BROWSER-SYNC (STATIC SERVER)
+// ---------------------------------------------------------------- 
+gulp.task('browser-sync', function () {
+  browserSync.init({
+    server: {
+      baseDir: './build'
+    }
+  // proxy: {
+  //  host: 'dev/',
+  //  port: '80'
+  // }
+  })
+  gulp.watch('src/**/*.html', ['html'])
+  gulp.watch('src/stylus/**/*.styl', ['stylus'])
+  gulp.watch('src/js/**/*.js', ['js'])
+})
+
+// ---------------------------------------------------------------- 
+// SET DEFAULT TASK
+// ---------------------------------------------------------------- 
+gulp.task('default', ['browser-sync'], function () {})
